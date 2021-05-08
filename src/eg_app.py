8e34c7a0732f99c9a514fca7520f1f06b1383fc1
MarkHallett <git@markhallett.co.uk>
2021-05-08 20:52:52 +0100

    b_002b

--- src/eg_app.py
+++ src/eg_app.py
@@ -4,8 +4,11 @@ import sys
 
 from devtools import debug
 
+def b_002b():
+    pass
+
 if __name__ == '__main__':
-    print('xx')
+    print('x----------------------x')
     print(sys.version)
     print(sys.executable)
     debug('yyy')
